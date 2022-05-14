3c52a7b9039fa2965244a1748c7040723b6d8daf
Mohammad Maasir <maasir554@gmail.com>
2022-05-14 19:55:28 +0530

    Update geometry.py - area_triangle(a,b,c) function added.

--- src/vectogebra/geometry.py
+++ src/vectogebra/geometry.py
@@ -15,3 +15,16 @@ def distance(a,b):
     else :
         raise TypeError("distance() takes two vectors as arguments")
 
+# area of a triangle formed by theree position vector as its vertices
+def area_triangle(a,b,c):
+    if type(a) == type(b) == type(c) == vect :
+        p = a - b  
+        r = c - b
+        return ((r^p).mod)/2
+    else :
+        raise TypeError("area_triangle() takes three vectors as arguments")
+
+a = vect(0,2)
+b = vect(2,0)
+c = vect(0,0)
+print(area_triangle(a,c,b))
