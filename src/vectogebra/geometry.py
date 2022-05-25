279f0593c664cba2ed22aa420be1aefb3823b810
Mohammad Maasir <maasir554@gmail.com>
2022-05-25 16:34:21 +0530

    geometry.py : major updates
    - centroid(*args) function added.
    - class line defined.

--- src/vectogebra/geometry.py
+++ src/vectogebra/geometry.py
@@ -136,3 +136,221 @@ def coplanar(*args : vect):
     else :
         return True # as three points are always coplanar in sD space.
 
+
+# function to find centroid of a polygon :
+def centroid(*args : vect):
+    """
+        Function to find centroid of a polygon.
+
+        ---
+
+        args : position vectors of all the vertices of the polygon whose centroid is to be calculated.
+
+        ---
+
+        Returns : position vector of the centroid of the polygon.
+
+        ---
+
+        Copyright © 2022 Mohammad Maasir 
+
+    """
+    l = len(args)
+    S = vect(0,0,0)
+    for i in args :
+        S += i
+    G = S/l
+    return G
+
+
+# ------------------------------------------------------------------
+# . . . . . . . . . IMPORTANT GEOMETRY CLASSES . . . . . . . . . . . 
+# ------------------------------------------------------------------
+
+class line:
+    """
+    Python class for line in a 3D space.
+
+    ---
+    
+    ## Methods to define a line : -
+    ### 1. Point-direction form 
+    Syntax : `line(point = vector(x,y,z), direction =vector(a,b,c) )`
+
+    - you also can write `p`  or `pt`  instead of `point` 
+
+    - and `d` or `dir` or `dr` instead of `direction` 
+    
+    ### 2. Two-points form 
+    Syntax : ` line(a,b) `
+    - here, `a` and `b` are position vectors of the two points through which the line is passing.
+    """
+
+    def __init__(self,a : vect = None,b : vect = None,**kwargs) :
+        
+        # Point-direction form :
+        
+        if a != None and b != None :
+            self.direction = b-a 
+            self.point = a
+
+        # two-point form :
+
+        if (a == None and b == None) and ('point' in kwargs) and ('direction' in kwargs) :
+            self.direction = kwargs['direction']
+            self.point = kwargs['point']
+
+        if (a == None and b == None) and ('p' in kwargs) and ('d' in kwargs) :
+            self.direction = kwargs['d']
+            self.point = kwargs['p']
+
+        if (a == None and b == None) and ('pt' in kwargs) and ('dir' in kwargs) :
+            self.direction = kwargs['dir']
+            self.point = kwargs['pt']
+    
+    
+    def __type__(self):
+        return "line"
+
+
+    # function to check if the line includes (or passes through) a point :
+    def includes(self,r : vect) :
+        """
+        Function to check if the line includes (or passes through) a point.
+
+        ---
+
+        argument : position vector of the point whose inclusion is to be checked.
+
+        ---
+
+        Returns : True if the line includes the point, False otherwise.
+
+        """
+        # by equation r = a + t*b {t is an arbitrary scalar number}
+        # we have (r-a) = t*b
+        # that means (r-a) should be proportional to b (direction vector)
+        # as cross product of propotional vectors (either parallel or anti-parallel) is 0, 
+        # we have :-
+
+        if (r-self.point)^self.direction == 0 :
+            return True
+        else :
+            return False
+
+
+    # function to check if two lines intersect or not :
+    def intersects(self,other) :
+        """
+        Function to check if two lines intersect or not.
+
+        ---
+
+        argument : another line object.
+
+        ---
+
+        Returns : True if the lines intersect, False otherwise.
+
+        """
+        # extracting vectors from the lines :
+        # ---
+        P1 = self.point
+        D1 = self.direction
+        # ---
+        P2 = other.point
+        D2 = other.direction
+        # ---
+        # --- --- --- 
+        
+        # extracting the components of vectors defined above.
+        # ---
+        p = P1.x
+        q = P1.y
+        r = P1.z
+        # ---
+        s = D1.x
+        t = D1.y
+        u = D1.z
+        # ---
+        # ---
+        a = P2.x
+        b = P2.y
+        c = P2.z    
+        # ---
+        f = D2.x
+        g = D2.y
+        h = D2.z
+        # ---
+        # --- --- ---
+        
+        # by mathematical calculations following condition is observed :
+        if (f*t != s*g) or (u*f != s*h) or (u*g != t*h) :
+            return True
+        else :
+            return False
+        
+        # this took me 2 hours to figure out the logic !
+
+    def intersection(self,other) :
+        """
+            Function to find the position vector of the intersection point (if exists), of two lines.
+
+            ---
+
+            The argument should also be a line object.
+        """
+        # extracting vectors from the lines :
+        # ---
+        P1 = self.point
+        D1 = self.direction
+        # ---
+        P2 = other.point
+        D2 = other.direction
+        # ---
+        # --- --- --- 
+
+        # extracting the components of vectors defined above.
+        # ---
+        p = P1.x
+        q = P1.y
+        r = P1.z
+        # ---
+        s = D1.x
+        t = D1.y
+        u = D1.z
+        # ---
+        # ---
+        a = P2.x
+        b = P2.y
+        c = P2.z    
+        # ---
+        f = D2.x
+        g = D2.y
+        h = D2.z
+        # ---
+        # --- --- ---
+        if self.intersects(other) : 
+            if (f*t) != (s*g) :
+                phi = ((t*(p-a))-(s*(q-b)))/((f*t)-(s*g))
+            elif (u*f) != (s*h) :
+                phi = ((u*(p-a))-(s*(r-c)))/((u*f)-(s*h))
+            elif (u*g) != (t*h) :
+                phi = ((u*(q-b))-(t*(r-c)))/((u*g)-(t*h))
+            else :
+                raise ValueError("Error in function line.intersects().")
+
+            INTERSECTION = P2 + (phi*D2)
+            return INTERSECTION
+
+        else : 
+            if (s/f) == (t/g) == (u/h) :
+                print("The lines are parallel !")
+                return None
+            else :
+                print("The lines are skew lines !")
+                return None
+        # this function in not fully perfect as it an not yet detect skew lines.
+        # it will still work for the lines that intersects each other
+        # and th function intersects() is also not perfect. it can not detect skew lines
+
